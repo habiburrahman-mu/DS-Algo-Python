cca6cadd0438cafe7f3e26d9f9943e7239cdc7c1
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2021-05-21 20:11:05 +0600

    Create array_.py

---
+++ array_.py
@@ -0,0 +1,17 @@
+# List create
+
+list1 = ['Physics', 'Chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5]
+list3 = ['a', 'b', 'c', 'd']
+
+# Accessing Values in Lists
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
+
+# Updating Lists
+list1[0] = "Biology"
+print(list1)
+
+# Delete List Elements
+del list1[1]
+print(list1)
